015193107daf79502d1e79b5865dc722d7c76c77
Christian Scholtysik <christianscholtysik@gmail.com>
2022-06-28 11:57:50 +0200

    change calendar, add more features to calendar, change pagination

--- src/components/DateTime.tsx
+++ src/components/DateTime.tsx
@@ -3,6 +3,25 @@ import { IonDatetime } from "@ionic/react";
 import "./main.css";
 
 const DateTime: React.FC = () => {
-  return <IonDatetime color="primary"></IonDatetime>;
+  const isWeekday = (dateString: string) => {
+    const date = new Date(dateString);
+    const utcDay = date.getUTCDay();
+
+    /**
+     * Date will be enabled if it is not
+     * Sunday or Saturday
+     */
+    return utcDay !== 0 && utcDay !== 6;
+  };
+  return (
+    <IonDatetime
+      color="primary"
+      minuteValues="0,15,30,45"
+      isDateEnabled={isWeekday}
+      showDefaultButtons={true}
+      min="2022-01-01T00:00:00"
+      max="2222-12-31T23:59:59"
+    ></IonDatetime>
+  );
 };
 export default DateTime;
