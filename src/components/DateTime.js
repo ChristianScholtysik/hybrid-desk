5985e89ffa5c2defb74eb7474d5302fbf1d839a7
Ivana <rubicode01@gmail.com>
2022-07-04 13:26:05 +0200

    change bounce arrows

--- src/components/DateTime.js
+++ src/components/DateTime.js
@@ -19,21 +19,23 @@ const DateTime = () => {
   };
   return (
     <>
-      <IonDatetime
-        color="primary"
-        // displayFormat="YYYY-MM-DD"
-        presentation="date"
-        // minuteValues="0,15,30,45"
-        isDateEnabled={isWeekday}
-        showDefaultButtons={true}
-        min="2022-01-01"
-        max="2222-12-31"
-        onIonChange={(e) => {
-          // console.log(e.detail.value.split("T")[0]);
-          setDate(e.detail.value.split("T")[0]);
-        }}
-      ></IonDatetime>
-      <div className="arrow bounce"></div>
+      <div>
+        <IonDatetime
+          color="primary"
+          // displayFormat="YYYY-MM-DD"
+          presentation="date"
+          // minuteValues="0,15,30,45"
+          isDateEnabled={isWeekday}
+          showDefaultButtons={true}
+          min="2022-01-01"
+          max="2222-12-31"
+          onIonChange={(e) => {
+            // console.log(e.detail.value.split("T")[0]);
+            setDate(e.detail.value.split("T")[0]);
+          }}
+        ></IonDatetime>
+        <div className="arrow bounce"></div>
+      </div>
     </>
   );
 };
